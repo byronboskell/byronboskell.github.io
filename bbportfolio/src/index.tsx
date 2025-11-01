b656a0373b0010022b7177e5ce0400e471dacd00
ByronBoskellWork <byron.boskell@ivai.com.au>
2025-11-01 11:28:11 +1030

    index: removing unnecessary components

--- bbportfolio/src/index.tsx
+++ bbportfolio/src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 import { ThemeProvider } from '@mui/material';
 import { theme } from "./theme/index";
+
+import WebsiteRouter from './website-router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <WebsiteRouter />
     </ThemeProvider>
   </React.StrictMode>
 );
